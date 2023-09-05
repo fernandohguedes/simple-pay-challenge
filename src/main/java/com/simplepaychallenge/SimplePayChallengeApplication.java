027e03b5519ea30b5b0cc065c310068caadc26da
Fernando Guedes <fernando.guedes@cwi.com.br>
2023-09-04 23:50:34 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/simplepaychallenge/SimplePayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.simplepaychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePayChallengeApplication.class, args);
+	}
+
+}
